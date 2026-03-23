8b0b43092d58adfe2af50313158b677c8df77cf2
JonathanInTheClouds <48375298+JonathanInTheClouds@users.noreply.github.com>
2026-03-22 22:19:10 -0500

    feat: update govault to v0.3.0

--- Formula/govault.rb
+++ Formula/govault.rb
@@ -1,13 +1,13 @@
 class Govault < Formula
   desc "A secure, encrypted password manager for the terminal"
   homepage "https://github.com/JonathanInTheClouds/govault"
-  version "0.2.3"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/JonathanInTheClouds/govault/releases/download/v0.2.3/govault-darwin-arm64"
-      sha256 "2e970f9eedef61c0974b2ddd32fb266acf8549667f4b168401d1d4c9a923dd0c"
+      url "https://github.com/JonathanInTheClouds/govault/releases/download/v0.3.0/govault-darwin-arm64"
+      sha256 "e35621d39fb014bec7d9dcea552a474a9114c0394e3a546628327a7f2c01cbbb"
 
       def install
         bin.install "govault-darwin-arm64" => "govault"
@@ -15,8 +15,8 @@ class Govault < Formula
     end
 
     on_intel do
-      url "https://github.com/JonathanInTheClouds/govault/releases/download/v0.2.3/govault-darwin-amd64"
-      sha256 "6bb357d4435dff02be6e05c97a11fb242dd87fb55648b155355f367d6dbb8d3f"
+      url "https://github.com/JonathanInTheClouds/govault/releases/download/v0.3.0/govault-darwin-amd64"
+      sha256 "e0a253015545c3e84da966abfeb9f176edcf50eb13e682c4555a6d52f66ede06"
 
       def install
         bin.install "govault-darwin-amd64" => "govault"
@@ -26,8 +26,6 @@ class Govault < Formula
 
   test do
     assert_match "GoVault", shell_output("#{bin}/govault --help")
-
-    # Hash a known string to verify the binary works
     assert_match version.to_s, shell_output("#{bin}/govault --version")
   end
 end
